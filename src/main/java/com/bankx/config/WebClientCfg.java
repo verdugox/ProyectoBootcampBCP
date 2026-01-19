c2b5b0c0dde46eed7aa8151be234378065f04353
verdugox <luisandresverdugox@gmail.com>
2026-01-19 10:41:59 -0500

    feat: Subiendo correcto url del mock webclient v2

--- src/main/java/com/bankx/config/WebClientCfg.java
+++ src/main/java/com/bankx/config/WebClientCfg.java
@@ -1,5 +1,6 @@
 package com.bankx.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
