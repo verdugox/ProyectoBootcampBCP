40ec578d739370ab609b2ae6c93c331b3838635d
verdugox <luisandresverdugox@gmail.com>
2026-01-19 10:59:33 -0500

    feat: Subiendo correcto url del mock webclient v4

--- src/main/java/com/bankx/config/WebClientCfg.java
+++ src/main/java/com/bankx/config/WebClientCfg.java
@@ -1,9 +1,14 @@
 package com.bankx.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientCfg {
@@ -20,5 +25,4 @@ public class WebClientCfg {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
-
 }
